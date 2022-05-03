5fbaef9fada481330a140fdd1e71d9524d3e0b47
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-03 09:58:54 +0530

    Add File Configuration

---
+++ AgriProductTracker.Data/Data/AgriProductTrackerDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriProductTracker.Data.Data
+{
+    public class AgriProductTrackerDbContext : DbContext
+    {
+        public AgriProductTrackerDbContext()
+        {
+
+        }
+
+        public AgriProductTrackerDbContext(DbContextOptions<AgriProductTrackerDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
