25b806d6952b8b3cd1a8d00bdd2c870d0f74a8a9
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-05 08:41:15 +0530

    Util and Custom View Model Added

---
+++ AgriProductTracker.ViewModel/DownloadFileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriProductTracker.ViewModel
+{
+    public class DownloadFileViewModel
+    {
+        public byte[] FileData { get; set; }
+        public string FileName { get; set; }
+    }
+}
