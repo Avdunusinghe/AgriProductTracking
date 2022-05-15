a5c8ebe831b72850876c774a32e76ff5f99cbb64
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-15 19:05:37 +0530

    Add Payment Service Configuration

---
+++ AgriProductTracking.PaymentService/Controllers/PaymentServiceController.cs
@@ -0,0 +1,84 @@
+﻿using AgriProductTracking.PaymentService.ViewModel;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using AuthorizeNet.Api.Controllers;
+using AuthorizeNet.Api.Contracts.V1;
+using Microsoft.AspNetCore.Mvc;
+using AuthorizeNet.Api.Controllers.Bases;
+
+namespace AgriProductTracking.PaymentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentServiceController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public PaymentServiceController(IConfiguration _configuration)
+        {
+            this._configuration = _configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Payment(PymentViewModel model)
+        {
+			Console.WriteLine("Charge Credit Card Sample");
+
+			ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.SANDBOX;
+
+			// define the merchant information (authentication / transaction id)
+			ApiOperationBase<ANetApiRequest, ANetApiResponse>.MerchantAuthentication = new merchantAuthenticationType()
+			{
+				name = _configuration["LoginId"],
+				ItemElementName = ItemChoiceType.transactionKey,
+				Item = _configuration["Transactionkey"]
+			};
+
+			var creditCard = new creditCardType
+			{
+				cardNumber = model.CardNumber,
+				expirationDate = model.ExpiratationDate,
+				cardCode = model.CardCode
+			};
+
+			//standard api call to retrieve response
+			var paymentType = new paymentType { Item = creditCard };
+
+			var transactionRequest = new transactionRequestType
+			{
+				transactionType = transactionTypeEnum.authCaptureTransaction.ToString(),   // charge the card
+				amount = 133.45m,
+				payment = paymentType
+			};
+
+			var request = new createTransactionRequest { transactionRequest = transactionRequest };
+
+			// instantiate the contoller that will call the service
+			var controller = new createTransactionController(request);
+			controller.Execute();
+
+			// get the response from the service (errors contained if any)
+			var response = controller.GetApiResponse();
+
+			if (response.messages.resultCode == messageTypeEnum.Ok)
+			{
+				if (response.transactionResponse != null)
+				{
+					Console.WriteLine("Success, Auth Code : " + response.transactionResponse.authCode);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Error: " + response.messages.message[0].code + "  " + response.messages.message[0].text);
+				if (response.transactionResponse != null)
+				{
+					Console.WriteLine("Transaction Error : " + response.transactionResponse.errors[0].errorCode + " " + response.transactionResponse.errors[0].errorText);
+				}
+			}
+			return Ok(response);
+		}
+    }
+}
---
+++ AgriProductTracking.PaymentService/ViewModel/PymentViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AgriProductTracking.PaymentService.ViewModel
+{
+    public class PymentViewModel
+    {
+        public string CardNumber { get; set; }
+        public string ExpiratationDate { get; set; }
+        public string Amount { get; set; }
+        public string CardCode { get; set; }
+    }
+}
