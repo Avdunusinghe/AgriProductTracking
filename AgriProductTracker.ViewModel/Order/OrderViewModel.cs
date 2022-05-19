10fa498ef2854af9edf58723698e8c7432ec8227
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-19 11:42:51 +0530

    GetOrderById Service Bug Fixed

--- AgriProductTracker.ViewModel/Order/OrderViewModel.cs
+++ AgriProductTracker.ViewModel/Order/OrderViewModel.cs
@@ -13,13 +13,11 @@ namespace AgriProductTracker.ViewModel.Order
             OrderItems = new List<OrderItemViewModel>();
         }
 
-       
-
         public int Id { get; set; }
         public decimal Amount { get; set; }
         public int CutomerId { get; set; }
         public string CutomerName { get; set; }
-        public int DeliveryServiceId { get; set; }
+        public int DeliveryPartnerId { get; set; } 
         public DateTime DateTime { get; set; }
         public bool IsProcessed  { get; set; }
         public string ShippingAdderess { get; set; }
