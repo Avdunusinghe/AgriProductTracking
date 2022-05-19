9f17ae7db9f41b5e24baf89b993eda8975636f82
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-19 21:43:16 +0530

    Configure SMS Service And SOA

--- AgriProductTracking.PaymentService/Controllers/PaymentServiceController.cs
+++ AgriProductTracking.PaymentService/Controllers/PaymentServiceController.cs
@@ -23,6 +23,7 @@ namespace AgriProductTracking.PaymentService.Controllers
 		private readonly IConfiguration _configuration;
 		private readonly ICurrentUserService _curretUserService;
 		private readonly IIdentityService _identityService;
+		private readonly IPaymentService _paymentService;
 		private readonly AgriProductTrackerDbContext _db;
 
 		public PaymentServiceController
@@ -30,12 +31,14 @@ namespace AgriProductTracking.PaymentService.Controllers
 			IConfiguration _configuration, 
 			ICurrentUserService _curretUserService,
 			IIdentityService _identityService,
+			IPaymentService _paymentService,
 		    AgriProductTrackerDbContext _db
 		)
         {
 			this._configuration = _configuration;
 			this._curretUserService = _curretUserService;
 			this._identityService = _identityService;
+			this._paymentService = _paymentService;
 			this._db = _db;
         }
        
@@ -43,123 +46,12 @@ namespace AgriProductTracking.PaymentService.Controllers
         [HttpPost]
 		public async Task<IActionResult> Payment(OrderContainerViewModel model)
         {
-			var customeOrderResponse = new CustomerOrderResponseViewModel();
-
-            try
-            {
-				string userName = string.Empty;
-
-				userName = _identityService.GetUserName();
-
-				var logggedInUser = _curretUserService.GetUserByUsername(userName);
-
-				var order = new Order()
-				{
-					TotalPrice = model.Amount,
-					CustomerId = logggedInUser.Id,
-					DateTime = DateTime.UtcNow,
-					IsProceesed = false,
-					ShippingAddress = model.ShippingAddress,
-					PostalCode = model.PostalCode,
-					City = model.City,
-
-				};
-
-				order.OrderItems = new HashSet<OrderItem>();
-
-				foreach (var item in model.OrderItems)
-				{
-					var productItem = new OrderItem()
-					{
-						OrderId = order.Id,
-						ProductId = item.Id,
-						NumberOfItems = item.Quantity,
-					};
-
-					order.OrderItems.Add(productItem);
-				}
-
-				_db.Orders.Add(order);
-
-				await _db.SaveChangesAsync();
-
-				customeOrderResponse.IsSuccess = true;
-				customeOrderResponse.CustomerEmail = logggedInUser.Email;
-				customeOrderResponse.CustomerMobileNumber = logggedInUser.MobileNumber;
-
-				//if (model.PaymentType == PaymentType.CreditCard)
-				//{
-
-				//	ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.SANDBOX;
-
-				//	// define the merchant information (authentication / transaction id)
-				//	ApiOperationBase<ANetApiRequest, ANetApiResponse>.MerchantAuthentication = new merchantAuthenticationType()
-				//	{
-				//		name = _configuration["LoginId"],
-				//		ItemElementName = ItemChoiceType.transactionKey,
-				//		Item = _configuration["Transactionkey"]
-				//	};
-
-				//	var creditCard = new creditCardType
-				//	{
-				//		cardNumber = model.CardNumber,
-				//		expirationDate = model.ExperationDate,
-				//		cardCode = model.Cvv
-				//	};
-
-				//	//standard api call to retrieve response
-				//	var paymentType = new paymentType { Item = creditCard };
-
-				//	var transactionRequest = new transactionRequestType
-				//	{
-				//		transactionType = transactionTypeEnum.authCaptureTransaction.ToString(),
-				//		amount = model.Amount,
-				//		payment = paymentType
-				//	};
-
-				//	var request = new createTransactionRequest { transactionRequest = transactionRequest };
-
-				//	// instantiate the contoller that will call the service
-				//	var controller = new createTransactionController(request);
-				//	controller.Execute();
-
-				//	// get the response from the service (errors contained if any)
-				//	var response = controller.GetApiResponse();
-
-				//	if (response.messages.resultCode == messageTypeEnum.Ok)
-				//	{
-				//		if (response.transactionResponse != null)
-				//		{
-				//			customeOrderResponse.IsSuccess = true;
-				//			customeOrderResponse.CustomerEmail = logggedInUser.Email;
-				//			customeOrderResponse.CustomerMobileNumber = logggedInUser.MobileNumber;
-
-				//		}
-				//	}
-				//	else
-				//	{
-				//		if (response.transactionResponse != null)
-				//		{
-				//			customeOrderResponse.IsSuccess = false;
-				//			customeOrderResponse.Message = "Payment Details Error Please try Again";
-				//		}
-				//	}
-
-				//}
-				//else
-				//{
-				//	customeOrderResponse.IsSuccess = false;
-				//	customeOrderResponse.CustomerEmail = logggedInUser.Email;
-				//	customeOrderResponse.CustomerMobileNumber = logggedInUser.MobileNumber;
-				//}
-
-			}
-			catch (Exception ex)
-            {
-
-            }
 			
-			return Ok(customeOrderResponse);
+			var userName = _identityService.GetUserName();
+
+			var response = await _paymentService.Payment(model, userName);
+			
+			return Ok(response);
 		}
 
 
---
+++ AgriProductTracking.PaymentService/Infrastructure/Services/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using AgriProductTracker.ViewModel;
+using AgriProductTracker.ViewModel.Order;
+
+namespace AgriProductTracking.PaymentService.Infrastructure.Services
+{
+    public interface IPaymentService
+    {
+        Task<CustomerOrderResponseViewModel> Payment(OrderContainerViewModel model, string userName);
+    }
+}
